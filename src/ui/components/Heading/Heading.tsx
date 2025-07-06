c9bd50077dde9700d1100663af25f70ed8eecf99
Denis Pitsul <denia.pitsul@gmail.com>
2025-07-06 21:11:53 +0300

    add fonts, typography styles, and use colors in :root

--- src/ui/components/Heading/Heading.tsx
+++ src/ui/components/Heading/Heading.tsx
@@ -1,10 +1,15 @@
 import './Heading.scss';
 
+type Tag = 'h1' | 'h2' | 'h3' | 'h4';
+
 type Props = {
+  tag: Tag;
   title: string;
 };
 
 // eslint-disable-next-line react/prop-types
-export const Heading: React.FC<Props> = ({ title }) => {
-  return <h1>{title}</h1>;
+export const Heading: React.FC<Props> = ({ tag, title }) => {
+  const Tag = tag;
+
+  return <Tag className="typography typography--h1">{title}</Tag>;
 };
