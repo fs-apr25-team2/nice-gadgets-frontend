05af42a9a95c6fe0d98280633eb2f945d127bdf6
Yurii Ivanenko <ura1408280594@gmail.com>
2025-07-07 15:10:02 +0300

    feat(DEV-9): add primary button with selected state

--- src/ui/components/Button/Button.tsx
+++ src/ui/components/Button/Button.tsx
@@ -1,5 +1,27 @@
 import './Button.scss';
+import cn from 'classnames';
+import React from 'react';
 
-export const Button = () => {
-  return <button type="button">Button</button>;
+type ButtonProps = {
+  selected?: boolean;
+  children?: React.ReactNode;
+  onClick?: () => void;
+};
+
+export const Button: React.FC<ButtonProps> = ({
+  selected = false,
+  children = 'Button',
+  onClick,
+}) => {
+  return (
+    <button
+      type="button"
+      className={cn('button', 'button--primary', {
+        'button--primary-selected': selected,
+      })}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 };
