cc2b18e7d86660550ad62e53a7edec8502024ad4
Denis Pitsul <denia.pitsul@gmail.com>
2025-07-10 18:49:48 +0300

    add product page about and tech specs, refactored AppLayout, use Breadcrumbs in Catalog page

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import './Footer.scss';
 export const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer__container">
+      <div className="footer__container page-container">
         <NavLink
           to="/"
           aria-label="Go to homepage"
