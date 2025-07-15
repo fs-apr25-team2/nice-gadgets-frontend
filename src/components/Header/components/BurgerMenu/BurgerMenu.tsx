904f8f7b68b35163a31fbc70a72500e4021360cc
Oksana Kuziv <oksanakuziv25@gmail.com>
2025-07-16 01:29:15 +0200

    feat(DEV-71) implement user menu

--- src/components/Header/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/Header/components/BurgerMenu/BurgerMenu.tsx
@@ -1,6 +1,7 @@
 import { HeaderIcons } from '../HeaderIcons/HeaderIcons';
 import { LangSwitcher } from '../LangSwitcher';
 import { NavigationTabs } from '../NavigationTabs/NavigationTabs';
+import { UserMenu } from '../UserMenu';
 import './BurgerMenu.scss';
 
 interface BurgerMenuProps {
@@ -21,6 +22,9 @@ export const BurgerMenu = ({ onClose }: BurgerMenuProps) => {
       </nav>
       <div className="burger-menu__lang">
         <LangSwitcher mobile={true} />
+      </div>
+      <div className="burger-menu__user">
+        <UserMenu />
       </div>
       <div className="burger-menu__icons">
         <HeaderIcons onClick={onClose} />
